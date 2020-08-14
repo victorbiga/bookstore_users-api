8446a5769f9c572e71bc84f5dd5838adbac2c9c0
VictorBiga <victor.biga@gmail.com>
2020-08-14 14:47:36 +0100

    added 200 ping controller

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,5 +1,7 @@
 package app
 
-func mapUrls()  {
-	
+import "github.com/victorbiga/bookstore_users-api/controllers"
+
+func mapUrls() {
+	router.GET("/ping", controllers.Ping)
 }
---
+++ controllers/ping_controller.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Ping OK response message
+func Ping(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
+}
