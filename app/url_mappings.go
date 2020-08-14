65e5ec299f358f74d7842247e4ca042cbb602136
VictorBiga <victor.biga@gmail.com>
2020-08-14 15:39:32 +0100

    restructured controllers

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,12 +1,15 @@
 package app
 
-import "github.com/victorbiga/bookstore_users-api/controllers"
+import (
+	"github.com/victorbiga/bookstore_users-api/controllers/ping"
+	"github.com/victorbiga/bookstore_users-api/controllers/users"
+)
 
 func mapUrls() {
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping", ping.Ping)
 
-	router.POST("/users", controllers.CreateUser)
-	router.GET("/users/:userid", controllers.GetUser)
+	router.POST("/users", users.CreateUser)
+	router.GET("/users/:userid", users.GetUser)
 	// panic: 'search' in new path '/users/search' conflicts with existing wildcard ':userid' in existing prefix '/users/:userid'
 	// router.GET("/users/search", controllers.SearchUser)
 }
