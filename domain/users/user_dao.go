11efd7517bf1c27be15c816a518a01e83bb619d1
VictorBiga <victor.biga@gmail.com>
2020-08-15 20:25:51 +0100

    omg user validationgit add .!

---
+++ domain/users/user_dao.go
@@ -0,0 +1,40 @@
+package users
+
+import (
+	"fmt"
+
+	"github.com/victorbiga/bookstore_users-api/utils/errors"
+)
+
+var (
+	usersDB = make(map[int64]*User)
+)
+
+// Get user not found
+func (user *User) Get() *errors.RestErr {
+	result := usersDB[user.Id]
+	if result == nil {
+		return errors.NewNotFoundError(fmt.Sprintf("user %d mot found", user.Id))
+	}
+
+	user.Id = result.Id
+	user.FirstName = result.FirstName
+	user.LastName = result.LastName
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+
+	return nil
+}
+
+// Save user error
+func (user *User) Save() *errors.RestErr {
+	current := usersDB[user.Id]
+	if current != nil {
+		if current.Email == user.Email {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+		}
+		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	}
+	usersDB[user.Id] = user
+	return nil
+}
---
+++ domain/users/user_dto.go
@@ -0,0 +1,24 @@
+package users
+
+import (
+	"strings"
+
+	"github.com/victorbiga/bookstore_users-api/utils/errors"
+)
+
+type User struct {
+	Id          int64  `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	DateCreated string `json:"date_created"`
+}
+
+// Validate invalid email address
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
+}
