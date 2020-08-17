c224b59a9a0115729341ab05818f994f4fa2a76f
VictorBiga <victor.biga@gmail.com>
2020-08-17 11:05:59 +0100

    Friday updates

--- services/users_service.go
+++ services/users_service.go
@@ -1,23 +1,22 @@
 package services
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/victorbiga/bookstore_users-api/domain/users"
 	"github.com/victorbiga/bookstore_users-api/utils/errors"
 )
 
 // CreateUser new user
-func CreateUser(user users.User) (users.User, *errors.RestErr) {
-	return user, nil
-
-	var defaultUser users.User
-
-	return defaultUser, &errors.RestErr{
-		Status: http.StatusInternalServerError,
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
 	}
 
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 // GetUser get user
--- utils/errors/rest_error.go
+++ utils/errors/rest_error.go
@@ -17,3 +17,12 @@ func NewBadRequestError(message string) *RestErr {
 		Error:   "bad_request",
 	}
 }
+
+//NewNotFoundError error message
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
